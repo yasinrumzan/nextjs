9038c8ddf7ba4ee6032ec94b362be31933b2a50e
yasinrumzan <rumzanyasin@gmail.com>
2024-07-22 13:30:12 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -74,13 +74,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Hevey Huls{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Eareas Titan Mamoth
           </p>
         </a>
 
