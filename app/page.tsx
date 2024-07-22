3ee59f5a145e6d584a5a8bbe7bbe46f7791ffe68
yasinrumzan <rumzanyasin@gmail.com>
2024-07-22 13:56:08 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -91,16 +91,12 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Drones{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            Brutus Mechanic Saboteur Booster Defender Trickster Hyperion Crisis    
   );
 }
