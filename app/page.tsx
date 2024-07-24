25f359b2c574131e7d87095205e7b66dbf431d28
yasinrumzan <rumzanyasin@gmail.com>
2024-07-24 15:22:17 -0600

    Revert "Update page.tsx"
    This reverts commit 3ee59f5a145e6d584a5a8bbe7bbe46f7791ffe68.

--- app/page.tsx
+++ app/page.tsx
@@ -91,12 +91,16 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Drones{" "}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Brutus Mechanic Saboteur Booster Defender Trickster Hyperion Crisis    
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
