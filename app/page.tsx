aac0ba98b15536a884afe829f8f49a39910ffa97
yasinrumzan <rumzanyasin@gmail.com>
2024-07-24 16:00:14 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Sorry error
+            Amazing
           </p>
         </a>
       </div>
